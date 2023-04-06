d9317c1eafd6c66e9a6aa2ceb86c258b9255f0a0
mercyndolo612@gmail.com <mercyndolo612@gmail.com>
2023-04-06 17:02:32 +0300

    adds code to the js file

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-// let countDownDate = new Date("Apr 16, 2023 14:45:60").getTime();
+let countDownDate = new Date("Apr 19, 2023 14:45:60").getTime();
 
-// let loop = setInterval(function () {
-//   let now = new Date().getTime();
-//   let timeLeft = countDownDate - now;
+let loop = setInterval(function () {
+  let now = new Date().getTime();
+  let timeLeft = countDownDate - now;
 
-//   let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
-//   let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//   let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
-//   let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+  let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-//   document.getElementById("days").innerHTML = days + "<p style='margin:0px'>Days</p>"
-//   document.getElementById("hours").innerHTML = hours + "<p style='margin:0px'>Hours</p>";
-//   document.getElementById("minutes").innerHTML = minutes + "<p style='margin:0px'>Minutes</p>";
-//   document.getElementById("seconds").innerHTML = seconds + "<p style='margin:0px'>Seconds</p>";
-// }, 1000);
+  document.getElementById("days").innerHTML = `<span><h2>${days}</h2> <p>Days</p></span>`
+  document.getElementById("hours").innerHTML = `<span><h2>${hours}</h2> <p>Hours</p></span>`
+  document.getElementById("minutes").innerHTML =`<span><h2>${minutes}</h2> <p>Minutes</p></span>`
+  document.getElementById("seconds").innerHTML = `<span><h2>${seconds}</h2> <p>Seconds</p></span>`
+}, 1000);
