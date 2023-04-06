513c1d60de68ba8cbfe330b7ec58d5a077092fe0
Daniel Karani <danielevans.karani@gmail.com>
2023-04-06 14:31:37 +0300

    Initital commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+// let countDownDate = new Date("Apr 16, 2023 14:45:60").getTime();
+
+// let loop = setInterval(function () {
+//   let now = new Date().getTime();
+//   let timeLeft = countDownDate - now;
+
+//   let days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+//   let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+//   let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+//   document.getElementById("days").innerHTML = days + "<p style='margin:0px'>Days</p>"
+//   document.getElementById("hours").innerHTML = hours + "<p style='margin:0px'>Hours</p>";
+//   document.getElementById("minutes").innerHTML = minutes + "<p style='margin:0px'>Minutes</p>";
+//   document.getElementById("seconds").innerHTML = seconds + "<p style='margin:0px'>Seconds</p>";
+// }, 1000);
